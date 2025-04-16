5038d173f62fd155bd55a159ad0bc34565755448
Nasala Tamrakar <nasalatamrakar@macbookair.mynetworksettings.com>
2025-04-16 00:16:35 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from 'react'
+import {puppyList} from './data.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-     
-     
-    </>
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  //console.log("puppyList: ", puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  function handleClick() {
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+    return (
+      <div className="App">
+              {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={()=>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>
+              <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id} className="reglon">
+            {puppy.name}
+          </p>
+            </p>
+          );
+        })}
+    </div>
   )
 }
 
